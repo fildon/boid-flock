083a8f4d59f95fb35b9d0f4a51912b87fd4c8bb7
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-07-19 19:39:05 +0200

    Adds JSDoc to be rendered in storybook

--- src/boidFlock.tsx
+++ src/boidFlock.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 
 export interface BoidFlockProps {
+  /**
+   * Simple message string to be rendered
+   */
   message: string;
 }
 
+/**
+ * Renders a lovely message
+ */
 export function BoidFlock({ message }: BoidFlockProps): JSX.Element {
   return <span>{message}</span>;
 }
